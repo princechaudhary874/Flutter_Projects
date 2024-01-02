b94339d3a2f41f3104d80310e3b7516bb3d34df3
princechaudhary874 <princechaudhary0001001@gmail.com>
2024-01-02 21:23:50 +0545

    using http

---
+++ lib/secrets.dart
@@ -0,0 +1,1 @@
+const openWeatherAPIKey = 'bc50cb7c43e9765d96a5d2070d0df3d0';
--- lib/weather_app.dart
+++ lib/weather_app.dart
@@ -3,13 +3,30 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'hourly_forecast_item.dart';
 import 'addtional_items.dart';
+import 'package:weather_apps/secrets.dart';
 import 'package:http/http.dart' as http;
 
-class WeatherScreen extends StatelessWidget {
+class WeatherScreen extends StatefulWidget {
   const WeatherScreen({super.key});
 
+  @override
+  State<WeatherScreen> createState() => _WeatherScreenState();
+}
+
+class _WeatherScreenState extends State<WeatherScreen> {
+  @override
+  void initState() {
+    super.initState();
+    getCurrentWeather();
+  }
+
   Future getCurrentWeather() async {
-    http.get(Uri.parse());
+    String cityName = 'Kathmandu';
+    final result = await http.get(
+      Uri.parse(
+          'https://api.openweathermap.org/data/2.5/forecast?q=$cityName&APPID=$openWeatherAPIKey'),
+    );
+    print(result.body);
   }
 
   @override
